b2d0ba710678f3d98332f5eef167ab0c5a28963d
ixharati <harati.r@innovatechs.com>
2026-03-12 11:21:16 +0530

    10-03-26

--- 09-03-26/springboot-demo/src/main/java/com/example/springboot_demo/service/service.java
+++ 09-03-26/springboot-demo/src/main/java/com/example/springboot_demo/service/service.java
@@ -1,11 +1,11 @@
-package com.example.springboot_demo;
+package com.example.springboot_demo.service;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Main {
+public class service {
 
     @Value("${name}")
     private String name;
